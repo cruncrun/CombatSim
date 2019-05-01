8168b908463a2f80653ba3e5528f4a26cd39a37e
Kamil Otrebscki <crunchipsccc@gmail.com>
2019-05-01 16:50:07 +0200

    zmiany formatowania tekstu

--- SWG_sim/ConsoleWriter.cs
+++ SWG_sim/ConsoleWriter.cs
@@ -5,6 +5,7 @@ using Console = Colorful.Console;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Colorful;
 
 namespace SWG_sim
 {
@@ -23,36 +24,79 @@ namespace SWG_sim
 
         static public void ParticipantDetails(Character character)
         {
-            Console.WriteLine(character.Name +
-                " \tI: " + character.Initiative +
-                "\tS: " + character.Strength +
-                " \tObrażenia broni: " + (character.Weapon.BaseAttackPower + character.Weapon.AttackPowerDiceRolls) + "-" +
-                    (character.Weapon.BaseAttackPower + (character.Weapon.AttackPowerDiceSides * character.Weapon.AttackPowerDiceRolls)) + " * " + character.Weapon.AttacksPerTurn);
+            string baseText = "{0} \tI: {1}\tS: {2}\tObrażenia broni: {3}-{4} * {5}";
+            Formatter[] elements = new Formatter[]
+            {
+                new Formatter(character.Name, GetCharacterNameColor(character)),
+                new Formatter(character.Initiative, Color.White),
+                new Formatter(character.Strength, Color.White),
+                new Formatter(character.Weapon.MinimumDamage, Color.White),
+                new Formatter(character.Weapon.MaximumDamage, Color.White),
+                new Formatter(character.Weapon.AttacksPerTurn, Color.White),
+            };
+            Console.WriteLineFormatted(baseText, Color.LightGray, elements);
         }
 
         static public void RegularHitMesage(Attack attack)
         {
-            Console.WriteLine(attack.Character.Name + " zadaje " + attack.Damage + " punktów obrażeń. " +
-                       attack.Opponent.Name + " ma " + attack.Opponent.RemainingHitPoints + "/" + attack.Opponent.HitPoints + " punktów życia.");
+            string baseText = "{0} zadaje atakiem {1} punktów obrażeń. {2} ma {3}/{4} punktów życia.";
+            Formatter[] elements = new Formatter[]
+            {
+                new Formatter(attack.Character.Name, GetCharacterNameColor(attack.Character)),
+                new Formatter(attack.Damage, Color.White),
+                new Formatter(attack.Opponent.Name, GetCharacterNameColor(attack.Opponent)),
+                new Formatter(attack.Opponent.RemainingHitPoints, Color.White),
+                new Formatter(attack.Opponent.HitPoints, Color.White)
+            };
+            Console.WriteLineFormatted(baseText, Color.LightGray, elements);            
         }
 
         static public void CriticalHitMessage(Attack attack)
         {
-            Console.WriteLine(attack.Character.Name + " trafia krytycznie zadając " + attack.Damage + " punktów obrażeń! " +
-                       attack.Opponent.Name + " ma " + attack.Opponent.RemainingHitPoints + "/" + attack.Opponent.HitPoints + " punktów życia.", Color.DeepPink);
+            string baseText = "{0} trafia krytycznie zadając {1} punktów obrażeń. {2} ma {3}/{4} punktów życia.";
+            Formatter[] elements = new Formatter[]
+            {
+                new Formatter(attack.Character.Name, GetCharacterNameColor(attack.Character)),
+                new Formatter(attack.Damage, Color.DeepPink),
+                new Formatter(attack.Opponent.Name, GetCharacterNameColor(attack.Opponent)),
+                new Formatter(attack.Opponent.RemainingHitPoints, Color.White),
+                new Formatter(attack.Opponent.HitPoints, Color.White)
+            };
+            Console.WriteLineFormatted(baseText, Color.HotPink, elements);
         }
 
         static public void MissedAttackMessage(Attack attack)
         {
-            Console.WriteLine(attack.Character.Name + " próbuje zaatakować " + attack.Opponent.Name + ", ale nie trafia...", Color.WhiteSmoke);
+            string baseText = "{0} próbuje zaatakować {1}, ale nie trafia...";
+            Formatter[] elements = new Formatter[]
+            {
+                new Formatter(attack.Character.Name, GetCharacterNameColor(attack.Character)),
+                new Formatter(attack.Opponent.Name, GetCharacterNameColor(attack.Opponent))
+            };
+            Console.WriteLineFormatted(baseText, Color.DimGray, elements);            
         }
 
         static public void BattleSummary(List<Character> participants)
         {
             foreach (var character in participants)
             {
-                System.Console.WriteLine(character.Name + " " + character.RemainingHitPoints + "/" + character.HitPoints + ". \tZabitych wrogów: " + character.KillCount + ". \tZadane obrażenia: " + character.DamageDone + ", otrzymane obrażenia: " + character.DamageTaken);
+                string baseText = "{0} \t{1}/{2}\tZabitych wrogów: {3}. \tZadane obrażenia: {4}, otrzymane obrażenia: {5}";
+                Formatter[] elements = new Formatter[]
+                {
+                    new Formatter(character.Name, GetCharacterNameColor(character)),
+                    new Formatter(character.RemainingHitPoints, Color.LightGray),
+                    new Formatter(character.HitPoints, Color.LightGray),
+                    new Formatter(character.KillCount, Color.LightGray),
+                    new Formatter(character.DamageDone, Color.LightGray),
+                    new Formatter(character.DamageTaken, Color.LightGray)
+                };
+                Console.WriteLineFormatted(baseText, Color.LightGray, elements);
             }
+        }
+
+        static public Color GetCharacterNameColor(Character character)
+        {
+            return character.IsAttacker ? Color.Tomato : Color.SteelBlue;
         }
     }
 }
